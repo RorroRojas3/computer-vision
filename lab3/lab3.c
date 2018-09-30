48dffc003ecbc61f1ba92f5b4230fbeb2848307e
rorrorojas3 <rrojas@clemson.edu>
2018-09-29 22:13:56 -0400

    update

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -6,6 +6,7 @@
 #define EDGE 255
 #define NOT_EDGE 0
 
+/* READS IN IMAGE */
 unsigned char *read_in_image(int rows, int cols, char file_header[], FILE *image_file)
 {
 	// Variable Declaration Section
@@ -46,7 +47,7 @@ unsigned char *original_image_threshold(unsigned char *original_image, int origi
 
     for (c1 = 0; c1 < (original_image_rows * original_image_cols); c1++)
     {
-        if (original_image[c1] <= threshold)
+        if (original_image[c1] < threshold)
         {
             output_threshold_image[c1] = 255;
         }
@@ -175,16 +176,16 @@ void get_transitions(unsigned char *image, int image_rows, int image_cols, int r
 
     if (edge_to_nonedge == 1)
     {
-        *end_points++;
+        *end_points = 1;
     }
     if (edge_to_nonedge > 2)
     {
-        *branch_points++;
+        *branch_points = 2;
     }
 }
 
 /* THINNING OF ORIGINAL THRESHOLD IMAGE */
-void thinning(unsigned char *image, int image_rows, int image_cols)
+unsigned char *thinning(unsigned char *image, int image_rows, int image_cols)
 {
 	// Variable Declaration Section
 	int c1, row, col;
@@ -192,18 +193,20 @@ void thinning(unsigned char *image, int image_rows, int image_cols)
 	int index = 0;
 	int run_again  = 1;
     int end_points, branch_points;
-	unsigned char *thined_image;
+	unsigned char *thinned_image;
 	unsigned char *temp_image;
+    int marked_pixels = 0;
+    int ran_times = 0;
     end_points = branch_points = 0;
 
 	// Allocate memory for thined image
-	thined_image = (unsigned char *)calloc(image_rows * image_cols, sizeof(unsigned char));
+	thinned_image = (unsigned char *)calloc(image_rows * image_cols, sizeof(unsigned char));
 	temp_image = (unsigned char *)calloc(image_rows * image_cols, sizeof(unsigned char));
 
 	// Copy original image to allocated thined image
 	for (c1 = 0; c1 < (image_rows * image_cols); c1++)
 	{
-		thined_image[c1] = image[c1];
+		thinned_image[c1] = image[c1];
 		temp_image[c1] = image[c1];
 	}
 
@@ -212,39 +215,102 @@ void thinning(unsigned char *image, int image_rows, int image_cols)
 	{
         run_again = 0;
         is_pixel_marked = 0;
+        marked_pixels = 0;
+        ran_times++;
 
 		for (row = 1; row < (image_rows - 1); row++)
 		{
 			for (col = 1; col < (image_cols - 1); col++)
 			{
                 index = (row * image_cols) + col;
-                if (thined_image[index] == 255)
+                if (thinned_image[index] == 255)
                 {
-                    get_transitions(thined_image, image_rows, image_cols, row, col, &is_pixel_marked, &end_points, &branch_points);
+                    is_pixel_marked = 0;
+                    get_transitions(thinned_image, image_rows, image_cols, row, col, &is_pixel_marked, &end_points, &branch_points);
                     if (is_pixel_marked == 1)
                     {
                         index = (row * image_cols) + col;
                         temp_image[index] = 0;
                         run_again = 1;
+                        marked_pixels += 1;
                     }
                 }        
 			}
 		}
 
+        printf("Pixels Marked on round %d: %d pixels\n", ran_times, marked_pixels);
+
         for (c1 = 0; c1 < (image_rows * image_cols); c1++)
 		{
-			thined_image[c1] = temp_image[c1];
+			thinned_image[c1] = temp_image[c1];
 		}
 	}
-
-    save_image(thined_image, "thined_image.ppm", image_rows, image_cols);
-    
+    return thinned_image;
 }
 
-void roc(unsigned char *threshold_image, unsigned char *msf_image, int threshold_rows, int threshold_cols, int msf_rows, int msf_cols, char *file_name)
+/* RETURNS IMAGE DETAILING IF PIXEL IS END POINT OR BRANCH POINT */
+unsigned char *get_end_and_branch_points(unsigned char *image, int image_rows, int image_cols)
 {
     // Variable Declaration Section
-    FILE *file;
+    int row = 0;
+    int c1 =0;
+    int col = 0;
+    int index = 0;
+    int mark_pixel = 0;
+    int end_points = 0;
+    int branch_points = 0;
+    int num_of_endp = 0;
+    int num_of_branchp = 0;
+    unsigned char *end_and_branch_point_image;
+    unsigned char *thinned_with_points;
+
+    // Allocate memory for image
+    end_and_branch_point_image = (unsigned char *)calloc(image_rows * image_cols, sizeof(unsigned char));
+    thinned_with_points = (unsigned char *)calloc(image_rows * image_cols, sizeof(unsigned char));
+
+    for (c1 = 0; c1 < (image_rows * image_cols); c1++)
+    {
+        thinned_with_points[c1] = image[c1];
+    }
+
+    for (row = 1; row < (image_rows - 1); row++)
+    {
+        for (col = 1; col < (image_cols - 1); col++)
+        {
+            index = (row * image_cols) + col;
+            end_points = 0;
+            branch_points = 0;
+            if (image[index] == 255)
+            {
+                get_transitions(image, image_rows, image_cols, row, col, &mark_pixel, &end_points, &branch_points);
+                if (end_points == 1)
+                {
+                    end_and_branch_point_image[index] = 50;
+                    thinned_with_points[index] = 100;
+                    num_of_endp += 1;
+                }
+                if (branch_points == 2)
+                {
+                    end_and_branch_point_image[index] = 150;
+                    thinned_with_points[index] = 200;
+                    num_of_branchp += 1;
+                }
+           }
+        }
+    }
+
+    printf("Found Endpoints: %d | Found Brachpoints: %d\n", num_of_endp, num_of_branchp);
+
+    save_image(end_and_branch_point_image, "Endpoints and branchpoints.ppm", image_rows, image_cols);
+    save_image(thinned_with_points, "thinned_with_points.ppm", image_rows, image_cols);
+
+    return end_and_branch_point_image;
+}
+
+void roc(unsigned char *msf_image, unsigned char *end_and_branch_point_image, int msf_rows, int msf_cols, int end_rows, int end_cols, char *file_name)
+{
+    // Variable Declaration Section
+    FILE *file, *csv_file;
     int c1, c2;
     int rows, cols;
     int row1, col1;
@@ -252,6 +318,8 @@ void roc(unsigned char *threshold_image, unsigned char *msf_image, int threshold
     int threshold;
     int index;
     int found;
+    int end_points = 0;
+    int branch_points = 0;
     char current_character[2];
     char desired_character[2];
     unsigned char *temp_image;
@@ -270,8 +338,12 @@ void roc(unsigned char *threshold_image, unsigned char *msf_image, int threshold
     // Allocate memory for temporary image
     temp_image = (unsigned char *)calloc(msf_rows * msf_cols, sizeof(unsigned char));
 
+    // Create CSV file and write the header
+	csv_file = fopen("Truth Table.csv", "w");
+    fprintf(csv_file, "Threshold,TP,FP,FN,TN,TPR,FPR,PPV\n");
+
     /* THRESHOLD IMAGE BASED ON THRESHOLD */
-    for (c1 = 0; c1 < 256; c1 += 5)
+    for (c1 = 0; c1 <= 250; c1 += 5)
     {
         threshold = c1;
 
@@ -286,44 +358,95 @@ void roc(unsigned char *threshold_image, unsigned char *msf_image, int threshold
                 temp_image[c2] = 0;
             }
         }
+        
+        /* EXTRACT DATA REGARDING CHARACTER, COLUMNS, AND ROWS FROM GROUND TRUTH TEXT FILE */
+		while((fscanf(file, "%s %d %d\n", current_character, &cols, &rows)) != EOF)
+		{
+            end_points = 0;
+            branch_points = 0;
+            found = 0;
+
+		    for (row1 = rows - 7; row1 <= (rows + 7); row1++)
+		    {
+		        for (col1 = cols - 4; col1 <= (cols + 4); col1++)
+		        {
+		            index = (row1 * msf_cols) + col1;
+		            if (temp_image[index] == 255)
+		            {
+		                found = 1;
+		            }
+                    if (end_and_branch_point_image[index] == 50)
+                    {
+                        end_points += 1;
+                    }
+                    if (end_and_branch_point_image[index] == 150)
+                    {
+                        branch_points += 1;
+                    }
+		        }
+		    }
+
+   
+            if ((found == 1) && (end_points == 1) && (branch_points == 1))
+            {
+                found = 1;
+            }
+            else
+            {
+                found = 0;
+            }
+
+
+            if ((found == 1) && (strcmp(current_character, desired_character) == 0))
+            {
+                tp++;
+            }
+            if ((found == 1) && (strcmp(current_character, desired_character) != 0))
+            {
+                fp++;
+            }
+            if ((found == 0) && (strcmp(current_character, desired_character) == 0))
+            {
+                fn++;
+            }
+            if ((found == 0) && (strcmp(current_character, desired_character) != 0))
+            {
+                tn++;
+            }    
+		}
+        // Write values to CSV file
+		fprintf(csv_file, "%d,%d,%d,%d,%d,%.2f,%.2f,%.2f\n", threshold, 
+		tp, fp, fn, tn, tp/(double)(tp +fn ),fp/(double)(fp+tn), fp/(double)(tp+fp));
+		tp = fp = fn = tn = 0;
+		rewind(file);
     }
 
-    /* EXTRACT DATA REGARDING CHARACTER, COLUMNS, AND ROWS FROM GROUND TRUTH TEXT FILE */
-    while((fscanf(file, "%s %d %d\n", current_character, &cols, &rows)) != EOF)
-    {
-        for (row1 = rows - 7; row1 <= (rows + 7); row1++)
-        {
-            for (col1 = cols - 4; col1 <= (cols + 4); col1++)
-            {
-                index = (row1 * msf_cols) + col1;
-                if (temp_image[index] == 255)
-                {
-                    found = 1;
-                }
-            }
-        }
-    }
+
+    fclose(file);
+    fclose(csv_file);
 }
 
 int main(int argc, char *argv[])
 {
     // Variable Declaration Section
-    FILE *image_file; 
-    int IMAGE_ROWS, IMAGE_COLS, IMAGE_BYTES;
+    FILE *image_file, *msf_file; 
+    int IMAGE_ROWS, IMAGE_COLS, IMAGE_BYTES, MSF_ROWS, MSF_COLS, MSF_BYTES;
     char file_header[MAXLENGTH];
    // unsigned char *template_image;
     unsigned char *input_image;
-   // unsigned char *msf_image;
+    unsigned char *msf_image;
     unsigned char *original_image_with_threshold;
+    unsigned char *thinned_image;
+    unsigned char *end_and_branch_points_image;
 
     /* CHECKT HAT THE USER ENTER THE CORRECT NUMBER OF PARAMETERS */
-    if (argc != 5)
+    if (argc != 4)
     {
-        printf("Usage: ./executable image_file template_file ground_truth_file\n");
+        printf("Usage: ./executable image_file msf_file ground_truth_file\n");
         exit(1);
     }
 
-    /* OPEN IMAGE, TEMPLATE, AND MSF IMAGE FILES AND OBTEIN THEIR INFORMATION */
+    /* OPEN IMAGE AND MSF IMAGE FILES AND OBTEIN THEIR INFORMATION */
     image_file = fopen(argv[1], "rb");
     if (image_file == NULL)
     {
@@ -338,15 +461,40 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    msf_file = fopen(argv[2], "rb");
+    if (msf_file == NULL)
+    {
+        printf("Error, could not read MSF image\n");
+        exit(1);
+    }
+    fscanf(msf_file, "%s %d %d %d\n", file_header, &MSF_COLS, &MSF_ROWS, &MSF_BYTES);
+    if ((strcmp(file_header, "P5") != 0) || (MSF_BYTES != 255))
+    {
+        printf("Error, not a greyscale 8-bit PPM image\n");
+        fclose(image_file);
+        fclose(msf_file);
+        exit(1);
+    }
+
     /* ALLOCATE MEMORY AND READ IN INPUT IMAGE */
     input_image = read_in_image(IMAGE_ROWS, IMAGE_COLS, file_header, image_file);
+    msf_image = read_in_image(MSF_ROWS, MSF_COLS, file_header, msf_file);
 
     /* THRESHOLD ORIGINAL IMAGE AT VALUE 128 */
     original_image_with_threshold = original_image_threshold(input_image, IMAGE_ROWS, IMAGE_COLS);
 
-    thinning(original_image_with_threshold, IMAGE_ROWS, IMAGE_COLS);
+    /* THIN THRESHOLD IMAGE */
+    thinned_image = thinning(original_image_with_threshold, IMAGE_ROWS, IMAGE_COLS);
+
+    /* END AND BRANCHPOINT IMAGE */
+    end_and_branch_points_image = get_end_and_branch_points(thinned_image, IMAGE_ROWS, IMAGE_COLS);
     
+    /* CALCULATE ROC */
+    roc(msf_image, end_and_branch_points_image, MSF_ROWS, MSF_COLS, IMAGE_ROWS, IMAGE_COLS, argv[3]);
+
+    /* SAVE IMAGES */
     save_image(original_image_with_threshold, "original_128_threshold.ppm", IMAGE_ROWS, IMAGE_COLS);
+    save_image(thinned_image, "thinned_image.ppm", IMAGE_ROWS, IMAGE_COLS);
     
     return 0;
 }
--- lab3/run.sh
+++ lab3/run.sh
@@ -2,9 +2,8 @@
 FILE="lab3.c"
 OUTPUT="lab3"
 ARG1="parenthood.ppm"
-ARG2="parenthood_e_template.ppm"
+ARG2="msf_e.ppm"
 ARG3="ground_truth.txt"
-ARG4="msf_e.ppm"
 gcc -Wall -g $FILE -o $OUTPUT
-./$OUTPUT $ARG1 $ARG2 $ARG3 $ARG4
+./$OUTPUT $ARG1 $ARG2 $ARG3
 
